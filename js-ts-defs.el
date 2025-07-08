18cbc77b66f79206869c267719bf786e4d422821
Jackson Ray Hamilton <jackson@jacksonrayhamilton.com>
2025-07-08 12:46:06 -0700

    Add prerequisites to the Commentary section too

--- js-ts-defs.el
+++ js-ts-defs.el
@@ -32,6 +32,18 @@
 ;; parse JavaScript code and build a scope structure to accurately resolve
 ;; variable definitions.
 ;;
+;; These functions are designed to be used inside `js-ts-mode'.
+;;
+;; Prerequisites:
+;;
+;; 1. Install JavaScript tree-sitter grammar:
+;;    (add-to-list 'treesit-language-source-alist
+;;                 '(javascript "https://github.com/tree-sitter/tree-sitter-javascript" "v0.20.1"))
+;;    M-x treesit-install-language-grammar RET javascript RET
+;;
+;; 2. Enable `js-ts-mode' for JavaScript files:
+;;    (add-to-list 'major-mode-remap-alist '(javascript-mode . js-ts-mode))
+;;
 ;; Usage:
 ;;
 ;;   (add-hook 'js-ts-mode-hook
